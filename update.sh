ee8c1231e866965e5e2fc481e6df68a239dabce9
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-01-04 10:13:00 +0100

    split lines in index

--- update.sh
+++ update.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-wget https://www.htw-berlin.de/coronavirus -O coronavirus-index.html
+function split_lines_and_rename {
+  if [ $(uname -s) == "Darwin" ]; then
+    # needs gnu-sed on mac, brew install gnu-sed
+    echo "Mac"
+    gsed  's/>/>\n/g' index.html > $1.html
+  else
+    echo "not a Mac"
+    sed  's/>/>\n/g' index.html > $1.html
+  fi
+  rm index.html
+}
+
+wget https://www.htw-berlin.de/coronavirus -O index.html
+split_lines_and_rename coronavirus-index
 
 for faq in \
      faq-fuer-studierende \
@@ -9,18 +22,10 @@ for faq in \
      corona-infektionsmeldung-und-nachverfolgung \
      gebaeude-zugang-mit-karte \
      ; do
-  echo $faq
   wget https://www.htw-berlin.de/coronavirus/$faq/
-  if [ $(uname -s) == "Darwin" ]; then
-    # needs gnu-sed on mac, brew install gnu-sed
-    echo "Mac"
-    gsed  's/>/>\n/g' index.html > $faq.html
-  else
-    echo "not a Mac"
-    sed  's/>/>\n/g' index.html > $faq.html
-  fi
-  rm index.html
+  split_lines_and_rename $faq
 done
+
 git add .
 git commit -m "update $(date +%Y-%m-%d---%H-%M-%S)"
 git push origin main
